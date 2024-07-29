3a195f4e4a5f7a5d04fa3207c482c04c8b1a4fa4
Greenxertz <Texbar07@gmail.com>
2024-07-29 09:31:27 +0200

    page updates

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,12 +1,101 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CardComponent } from '../../element/card/card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, CardComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  tools = [
+    {
+      iconUrl: 'devicon-csharp-plain',
+      title: 'C#',
+    },
+    {
+      iconUrl: 'devicon-cplusplus-plain',
+      title: 'C++',
+    },
+    {
+      iconUrl: 'icon-delphi-svgrepo-com',
+      title: 'Delphi',
+    },
+    {
+      iconUrl: 'fa-solid fa-gear',
+      title: 'RESTful APIs',
+    },
+    {
+      iconUrl: 'devicon-mysql-plain-wordmark',
+      title: 'MySQL',
+    },
+    {
+      iconUrl: 'devicon-html5-plain-wordmark',
+      title: 'HTML5',
+    },
+    {
+      iconUrl: 'devicon-css3-plain-wordmark',
+      title: 'CSS3',
+    },
+    {
+      iconUrl: 'devicon-javascript-plain',
+      title: 'JavaScript',
+    },
+    {
+      iconUrl: 'devicon-php-plain',
+      title: 'PHP',
+    },
+    {
+      iconUrl: 'devicon-docker-plain',
+      title: 'Docker',
+    },
+    {
+      iconUrl: 'devicon-angularmaterial-plain',
+      title: 'Angular Material',
+    },
+    {
+      iconUrl: 'devicon-github-original',
+      title: 'Github',
+    },
+    {
+      iconUrl: 'devicon-git-plain',
+      title: 'Git',
+    },
+    {
+      iconUrl: 'fa-solid fa-book',
+      title: 'More to come!',
+    },
+  ];
 
+  frameworks = [
+    {
+      iconUrl: 'devicon-angular-plain',
+      title: 'Angular',
+    },
+    {
+      iconUrl: 'devicon-flutter-plain',
+      title: 'Flutter',
+    },
+  ];
+
+  hobbies = [
+    {
+      iconUrl: 'devicon-blender-original',
+      title: 'Blender',
+    },
+    {
+      iconUrl: 'icon-davinci-resolve-12',
+      title: 'Da Vinci Resolve',
+    },
+    {
+      iconUrl: 'icon-AutoCad_new_logo',
+      title: 'Auto CAD',
+    },
+    {
+      iconUrl: 'devicon-photoshop-plain',
+      title: 'Adobe Photoshop',
+    },
+  ];
 }
--- src/app/page/projects/projects.component.ts
+++ src/app/page/projects/projects.component.ts
@@ -1,12 +1,52 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ShowcasecardsComponent } from '../../element/showcasecards/showcasecards.component';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ShowcasecardsComponent],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.css'
+  styleUrl: './projects.component.css',
 })
 export class ProjectsComponent {
-
+  projects = [
+    {
+      imageUrl:
+        'https://ih1.redbubble.net/image.2480982209.7657/st,small,507x507-pad,600x600,f8f8f8.u2.jpg',
+      title: 'Guessing Game',
+      desc: 'A Game where the user guesses the song',
+      chips: [
+        'devicon-csharp-plain',
+        'fa-solid fa-gear',
+        'devicon-docker-plain',
+      ],
+    },
+    {
+      imageUrl:
+        'https://ih1.redbubble.net/image.2480982209.7657/st,small,507x507-pad,600x600,f8f8f8.u2.jpg',
+      title: 'Tukkies Tools',
+      desc: 'Ecommerce website project',
+      chips: [
+        'devicon-php-plain',
+        'devicon-mysql-plain-wordmark',
+        'devicon-css3-plain-wordmark',
+        'devicon-javascript-plain',
+        'devicon-html5-plain-wordmark',
+      ],
+    },
+    {
+      imageUrl:
+        'https://ih1.redbubble.net/image.2480982209.7657/st,small,507x507-pad,600x600,f8f8f8.u2.jpg',
+      title: 'Portfolio website',
+      desc: 'The website you are currently on',
+      chips: [
+        'devicon-css3-plain-wordmark',
+        'devicon-javascript-plain',
+        'devicon-html5-plain-wordmark',
+        'devicon-angularmaterial-plain',
+        'devicon-angular-plain',
+      ],
+    },
+  ];
 }
